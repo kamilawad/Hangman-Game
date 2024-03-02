f04ee0d8df7f50e1c14c6e2d77b85c364ba05f11
kamilawad1 <kamilawad15@gmail.com>
2024-03-03 01:12:33 +0200

    added the logic of the game

--- scripts/game.js
+++ scripts/game.js
@@ -1,30 +1,83 @@
 const result = document.getElementById('answer-section');
 const letters = document.querySelectorAll('.letter');
+const hangImg = document.getElementById("hang");
 
 const words = ['shawshankredemption', 'godfather', 'darknight', 'goodwillhunt', 'shutterisland',
  'lordoftherings', 'pulpfiction', 'fightclub', 'butterflyeffect', 'taxidriver'];
-let word = ''
-let dashesWord = '';
+const hangParts = [head, body, leftHand, rightHand,leftLeg, rightLeg];
+let word = '';
+let dashesWord = [];
+let nbFalseGuesses = 0;
 
 function startGame() {
     word = words[Math.floor(Math.random() * words.length)];
     for(let i = 0; i < word.length; i++){
-        dashesWord += '-';
+        dashesWord.push('-');
     }
-    result.innerHTML = dashesWord;
+    result.innerHTML = dashesWord.join('');
 
     for (let i = 0; i < letters.length; i++) {
-        letters[i].addEventListener('click', function(e) {
-            console.log(e.target.textContent.toLowerCase());
-        });
+        letters[i].addEventListener('click', letterClick);
     }
 
-    document.addEventListener('keydown', function(e) {
-        const key = e.key.toLowerCase();
-        if (key >= 'a' && key <= 'z') {
-            console.log(key);
+    document.addEventListener('keydown', letterClick);
+}
+
+function letterClick(e) {
+    let found = false;
+    for (let j = 0; j < word.length; j++) {
+        if (e.target.textContent.toLowerCase() == word[j]) {
+            dashesWord[j] = word[j];
+            result.innerHTML = dashesWord.join('');
+            found = true;
         }
-    });
+    }
+    if (!found) {
+        hangParts[nbFalseGuesses]();
+        nbFalseGuesses++;
+    }
+    setTimeout(isGameOver, 100);
+}
+
+function keyClick(e) {
+    let found = false;
+    for (let j = 0; j < word.length; j++) {
+        if (e.target.textContent.toLowerCase() == word[j]) {
+            dashesWord[j] = word[j];
+            result.innerHTML = dashesWord.join('');
+            found = true;
+        }
+    }
+    if (!found) {
+        hangParts[nbFalseGuesses]();
+        nbFalseGuesses++;
+    }
+    setTimeout(isGameOver, 100);
+}
+
+function isGameOver() {
+    if (nbFalseGuesses == 6) {
+        for (let i = 0; i < letters.length; i++) {
+            letters[i].removeEventListener('click', letterClick);
+            letters[i].addEventListener('click', letterClick);
+        }
+        nbFalseGuesses = 0;
+        dashesWord = [];
+        hangImg.innerHTML = '';
+        alert('You lose!! The word was:' + word);
+        startGame();
+    }
+    else if(dashesWord.join('') == word) {
+        alert('YOU WON! The word was:' + word);
+        nbFalseGuesses = 0;
+        dashesWord = [];
+        hangImg.innerHTML = '';
+        for (let i = 0; i < letters.length; i++) {
+            letters[i].removeEventListener('click', letterClick);
+            letters[i].addEventListener('click', letterClick);
+        }
+        startGame();
+    }
 }
 
 startGame();
