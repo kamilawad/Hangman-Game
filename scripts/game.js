6b31f938cda9ecf5e9ef69d2ebe9912287b4259a
kamilawad1 <kamilawad15@gmail.com>
2024-03-02 22:57:55 +0200

    added user click on letters

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,8 @@
 const result = document.getElementById('answer-section');
+const letters = document.querySelectorAll('.letter');
 
-const words = ['shawshankredemption', 'godfather', 'darknight', 'goodwillhunt', 'shutterisland', 'lordoftherings', 'pulpfiction', 'fightclub', 'butterflyeffect', 'taxidriver'];
+const words = ['shawshankredemption', 'godfather', 'darknight', 'goodwillhunt', 'shutterisland',
+ 'lordoftherings', 'pulpfiction', 'fightclub', 'butterflyeffect', 'taxidriver'];
 let word = ''
 let dashesWord = '';
 
@@ -10,6 +12,19 @@ function startGame() {
         dashesWord += '-';
     }
     result.innerHTML = dashesWord;
+
+    for (let i = 0; i < letters.length; i++) {
+        letters[i].addEventListener('click', function(e) {
+            console.log(e.target.textContent.toLowerCase());
+        });
+    }
+
+    document.addEventListener('keydown', function(e) {
+        const key = e.key.toLowerCase();
+        if (key >= 'a' && key <= 'z') {
+            console.log(key);
+        }
+    });
 }
 
 startGame();
